d29c6d8baa4f02dc2e8d6982f9adac1accab426a
Ripudaman Singh <rds0751@gmail.com>
2019-01-11 18:49:12 +0530

    Update urls.py

--- online_test/urls.py
+++ online_test/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     url(r'^grades/', include('grades.urls', namespace='grades',
                              app_name='grades')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
