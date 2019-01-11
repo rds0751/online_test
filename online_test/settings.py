354b6811b002e5451e4edfd870e2275d4b044d9d
Ripudaman Singh <rds0751@gmail.com>
2019-01-11 19:09:39 +0530

    Update settings.py

--- online_test/settings.py
+++ online_test/settings.py
@@ -68,10 +68,7 @@ WSGI_APPLICATION = 'online_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db('DATABASE_URL'),
 }
 
 
