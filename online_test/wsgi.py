6c857bd20392c39fd757a76eb80fd755ceba2f64
Ripudaman Singh <rds0751@gmail.com>
2019-01-11 18:48:20 +0530

    Update wsgi.py

--- online_test/wsgi.py
+++ online_test/wsgi.py
@@ -1,14 +1,17 @@
 """
-WSGI config for online_test project.
+WSGI config for Approapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_test.settings")
 
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
